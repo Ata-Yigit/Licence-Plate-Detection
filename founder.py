2fa6a14b02f5e21d67f424aac6dad27cebd7da40
Ata-Yigit <100075528+Ata-Yigit@users.noreply.github.com>
2023-07-18 17:16:46 +0300

    Create founder.py

---
+++ founder.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+img = cv.imread("C:/Users/user/Downloads/plaka4.jpg")
+img = cv.resize(img,(1600,900))
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+filtered = cv.bilateralFilter(gray,20,60,20)
+edges = cv.Canny(filtered,60,200)
+cv.imshow("asb",edges)
+contours,_ = cv.findContours(edges.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    approx = cv.approxPolyDP(cnt,20, True)
+    if len(approx) == 4 and cv.contourArea(cnt)>3000:
+        img = cv.drawContours(img, [approx], -1, (0,255,0), 3)
+cv.imshow("Plate",img)
+cv.waitKey(0)
+cv.destroyAllWindows()
