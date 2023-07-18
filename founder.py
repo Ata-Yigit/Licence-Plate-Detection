c090b777dd9b420f66b6c51de45dcf13d0c6075b
Ata-Yigit <100075528+Ata-Yigit@users.noreply.github.com>
2023-07-19 00:28:16 +0300

    Latest commit

--- founder.py
+++ founder.py
@@ -1,19 +1,23 @@
 import cv2 as cv
+import numpy as np
 import easyocr
 
-img = cv.imread("C:/Users/user/Downloads/plaka3.jpg")
+img = cv.imread("C:/Users/user/Downloads/plaka.jpg")
 img = cv.resize(img,(1600,900))
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 filtered = cv.bilateralFilter(gray,20,60,20)
 edges = cv.Canny(filtered,60,200)
 contours,_ = cv.findContours(edges.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+mask = np.zeros(gray.shape, np.uint8)
 for cnt in contours:
     approx = cv.approxPolyDP(cnt,20, True)
     if len(approx) == 4 and cv.contourArea(cnt)>3000:
-        img = cv.drawContours(img, [approx], -1, (0,255,0), 3)
-reader = easyocr.Reader(['en'])
-result = reader.readtext(img)
+        x,y,z,w = approx
+        new_img = cv.drawContours(mask, [approx],0,255, -1)
+        new_img = cv.bitwise_and(img, img,mask=mask)
+reader = easyocr.Reader(['lt'])
+result = reader.readtext(new_img)
 print(result)
-cv.imshow("Plate",img)
+cv.imshow("Plate",new_img)
 cv.waitKey(0)
 cv.destroyAllWindows()
